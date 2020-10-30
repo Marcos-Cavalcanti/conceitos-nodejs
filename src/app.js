245b100bb201e8d85a5abd6fe378626d49f5dbd3
Marcos Cavalcanti <marcosjcavalcanti.20@gmail.com>
2020-10-30 01:45:27 -0300

    Desafio feito

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@ app.delete("/repositories/:id", (request, response) => {
   const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
   
   if (repositoryIndex < 0) {
-    return response.status(400).json({ error: "Repository not found"})
+    return response.status(400).json({ error: "Repository not found"});
   }
 
   repositories.splice(repositoryIndex, 1);
